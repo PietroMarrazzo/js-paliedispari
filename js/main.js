8f475c9218e0179ce04c9a872fbaeb308be3ddc2
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-22 14:15:06 +0200

    pari dispari wip

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // scegliere tra pari o dispari
 var parola = prompt('Inserisci una parola');
 var scelta = prompt('Scegli tra pari o dispari');
-var numero = prompt('Inserisci un numero');
+var numero = prompt('Inserisci un numero tra 1 e 5, loro compresi');
 // console.log(parola, scelta, numero);
 
 
@@ -17,3 +17,19 @@ function reversePalindroma(word) {
     }
     return reverse + ' non palindroma';
 }
+
+
+
+
+
+// PARI O DISPARI
+if (numero < 1 || numero > 5) {
+    var numero = prompt('Inserisci un numero compreso tra 1 e 5, loro compresi');
+}
+
+// funzione generatore tiro computer 1-5
+function tiroPC(min, max) {
+    var tiroPC = Math.floor((Math.random() * max - min) + min);
+    return tiroPC;
+}
+
