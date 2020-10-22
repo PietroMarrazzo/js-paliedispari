92ec9ae1d00f8daea3746dd3311ecda28476e85c
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-22 14:36:36 +0200

    pari dispari wip 2
    fix somma tiri

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-// chiedere all'utente di inserire una parola, inserire un numero e 
-// scegliere tra pari o dispari
+// chiedere all'utente di inserire una parola, inserire un numero e scegliere tra pari o dispari
 var parola = prompt('Inserisci una parola');
 var scelta = prompt('Scegli tra pari o dispari');
-var numero = prompt('Inserisci un numero tra 1 e 5, loro compresi');
+var numero = parseInt(prompt('Inserisci un numero tra 1 e 5, loro compresi'));
 // console.log(parola, scelta, numero);
 
 
@@ -21,15 +20,42 @@ function reversePalindroma(word) {
 
 
 
-
 // PARI O DISPARI
 if (numero < 1 || numero > 5) {
-    var numero = prompt('Inserisci un numero compreso tra 1 e 5, loro compresi');
+    var numero = parseInt(prompt('Inserisci un numero compreso tra 1 e 5, loro compresi'));
 }
 
+var numberPC = tiroPC(1, 5);
+console.log(numberPC);
+
+var somma = numberPC + numero;
+console.log(somma);
+
+var result = pariDispari(somma);
+
+// console.log(result);
+
+// if (scelta = 'pari') {
+//     console.log('Hai vinto!');
+// } else if (scelta = 'dispari') {
+//     console.log('Hai perso :(');
+// }
+
+
+
+
+// FUNZIONI
+// funzione pari dispari
+function pariDispari(number) {
+    if (number % 2 == 0) {
+        return 'pari';
+    } else if (number % 2 != 0) {
+        return 'dispari';
+    }   
+}
 // funzione generatore tiro computer 1-5
 function tiroPC(min, max) {
-    var tiroPC = Math.floor((Math.random() * max - min) + min);
+    var tiroPC = Math.floor((Math.random() * max) + min);
     return tiroPC;
 }
 
